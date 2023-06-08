7cb8212b10d121e93e2c71f89fe15abdfe21162f
alejandro <alejandrober96@gmail.com>
2023-06-08 07:58:06 -0500

    Add active class

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,15 @@
+'use client'
+
 import Link from "next/link"
 import Image from "next/image"
 import Logo from "../assets/K2.png"
 import styles from "../styles/Navbar.module.css"
+import { usePathname } from "next/navigation"
 
 
 function Navbar() {
+  const path = usePathname()
+
     return (
       <nav className={styles.navbar}>
         <div className={styles.logoHolder}>
@@ -15,14 +20,32 @@ function Navbar() {
           />
         </div>
         <ul className={styles.links}>
-          <li>
-            <Link href="/"> Inicio </Link>
+          <li
+            className={path == "/" ? styles.active : ""}
+          >
+            <Link
+              href="/"
+            > 
+              Inicio 
+            </Link>
           </li>
-          <li>
-            <Link href="/sobre-nosotros"> Sobre Nosotros </Link>
+          <li
+            className={path == "/sobre-nosotros" ? styles.active : ""}
+          >
+            <Link
+              href="/sobre-nosotros"
+            >
+              Sobre Nosotros
+            </Link>
           </li>
-          <li>
-            <Link href="/contacto"> Contacto </Link>
+          <li
+            className={path == "/contacto" ? styles.active : ""}
+          >
+            <Link
+              href="/contacto"
+            >
+              Contacto
+            </Link>
           </li>
         </ul>
       </nav>
