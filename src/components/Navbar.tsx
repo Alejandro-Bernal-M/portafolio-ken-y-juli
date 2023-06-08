0def1ee8f3c59f0aa2d60ac800f1ccb7d40cad37
alejandro <alejandrober96@gmail.com>
2023-06-08 07:35:37 -0500

    Add styles to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,20 @@
 import Link from "next/link"
+import Image from "next/image"
+import Logo from "../assets/K2.png"
+import styles from "../styles/Navbar.module.css"
+
 
 function Navbar() {
     return (
-      <nav>
-        <span>
-          Logo
-        </span>
-        <ul>
+      <nav className={styles.navbar}>
+        <div className={styles.logoHolder}>
+          <Image 
+            className={styles.logo}
+            src= {Logo}
+            alt="Logo"
+          />
+        </div>
+        <ul className={styles.links}>
           <li>
             <Link href="/"> Inicio </Link>
           </li>
