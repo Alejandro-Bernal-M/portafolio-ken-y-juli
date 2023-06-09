b230c43fe9bd392bd6e95a84393c8f2a30455f35
alejandro <alejandrober96@gmail.com>
2023-06-09 07:22:12 -0500

    Add general layout for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,27 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import pareja from '../assets/pareja.png'
 
 export default function Home() {
   return (
     <main className={styles.main}>
      <h1>Kenyth y Juliana</h1>
+     <div className={styles.photoAndInfo}>
+      <div>
+        <Image src={pareja} alt="Pareja" className={styles.photo} />
+      </div>
+      <div className={styles.info}>
+        <h2>Estamos muy felices de compartir nuestro talento.</h2>
+        <br />
+        <p>Nuestros servicios:</p>
+        <ul>
+          <li>Ambientacion de eventos sociales.</li>
+          <li>Bailes tipicos Colombianos.</li>
+          <li>Coreografias para eventos sociales.</li>
+          <li>Clases de baile.</li>
+        </ul>
+      </div>
+     </div>
     </main>
   )
 }
