aad760958dfa100729db9766eee74b2cb78fa48f
alejandro <alejandrober96@gmail.com>
2023-06-19 11:58:43 -0500

    Correction of content on the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,20 +5,28 @@ import pareja from '../assets/pareja.png'
 export default function Home() {
   return (
     <main className={styles.main}>
-     <h1>KENYTH Y JULIANA</h1>
+     <h1>RAICES DE MI TIERRA</h1>
      <div className={styles.photoAndInfo}>
-      <div>
-        <Image src={pareja} alt="Pareja" className={styles.photo} />
-      </div>
+      <Image src={pareja} alt="Pareja" className={styles.photo} />
       <div className={styles.info}>
         <h2>¡Estamos muy felices de compartir nuestro talento!</h2>
         <br />
         <h3>Nuestros servicios:</h3>
-        <ul>
+        <ul className={styles.ourServices}>
           <li>Ambientación de eventos sociales.</li>
           <li>Bailes típicos Colombianos.</li>
-          <li>Coreografías para eventos sociales.</li>
-          <li>Clases de baile.</li>
+          <li>Coreografías para eventos artísticos y sociales.</li>
+          <li>Coreografía de danza contemporánea.</li>
+          <li>Clases de baile sociales.</li>
+        </ul>
+        <br />
+        <ul className={styles.genres}>
+          <li>Salsa.</li>
+          <li>Bachata.</li>
+          <li>Merengue.</li>
+          <li>Folclor colombiano.</li>
+          <li>Danza contemporanea.</li>
+          <li>Hip hop.</li>
         </ul>
       </div>
      </div>
